f357ecde519a2ac5f6d0335fb782cfaa3d45c3d5
Tara Bergener <tara.kelly211@gmail.com>
2026-01-24 23:14:19 -0500

    Message files and folders created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { DocumentsComponent } from './documents/documents.component';
 import { DocumentListComponent } from './documents/document-list/document-list.component';
 import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
 import { DocumentItemComponent } from './documents/document-item/document-item.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageItemComponent } from './messages/message-item/message-item.component';
+import { MessageEditComponent } from './messages/message-edit/message-edit.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +29,10 @@ import { DocumentItemComponent } from './documents/document-item/document-item.c
     DocumentListComponent,
     DocumentDetailComponent,
     DocumentItemComponent,
+    MessagesComponent,
+    MessageItemComponent,
+    MessageEditComponent,
+    MessageListComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
