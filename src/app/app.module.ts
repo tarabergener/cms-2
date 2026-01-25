ecbfb0d42ca67aed061907a70760e0b8e133ce57
Tara Bergener <tara.kelly211@gmail.com>
2026-01-24 23:06:16 -0500

    Added document folder and files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,11 @@ import { HeaderComponent } from './header/header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
-import { ContactItemComponent } from './contact-item/contact-item.component';
+import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentItemComponent } from './documents/document-item/document-item.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +21,10 @@ import { ContactItemComponent } from './contact-item/contact-item.component';
     ContactListComponent,
     ContactDetailComponent,
     ContactItemComponent,
+    DocumentsComponent,
+    DocumentListComponent,
+    DocumentDetailComponent,
+    DocumentItemComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
