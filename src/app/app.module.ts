b7fc1359bdae69d2816de6fab53fafe7ca511d19
Tara Bergener <tara.kelly211@gmail.com>
2026-01-30 01:13:52 -0500

    Updated the user dropdown. Dropdown toggles open and closed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { MessageItemComponent } from './messages/message-item/message-item.component';
 import { MessageEditComponent } from './messages/message-edit/message-edit.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,7 @@ import { MessageListComponent } from './messages/message-list/message-list.compo
     MessageEditComponent,
     MessageListComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, DropdownDirective],
   providers: [],
   bootstrap: [AppComponent],
 })
