0098ffc3769df6618ea4ff8efdedfd1320549bc5
Tara Bergener <tara.kelly211@gmail.com>
2026-02-07 11:22:41 -0500

    Added new images for contacts in contact array. Created contact service. Implemented service in contacts component.

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Contact } from './contact.model';
+import { ContactService } from './contact.service';
 
 @Component({
   selector: 'cms-contacts',
   standalone: false,
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css',
+  providers: [ContactService],
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
+  @Output() contactWasSelected = new EventEmitter<Contact>();
   selectedContact: Contact | null = null;
+  contacts: Contact[];
 
-  constructor() {}
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.contacts = this.contactService.getContacts();
+  }
+
+  onContactSelected(contact: Contact) {
+    this.contactWasSelected.emit(contact);
+  }
 }
