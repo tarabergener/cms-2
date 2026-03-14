1a97d89efb71333f8ba69ee7423d28800926a4a9
Tara Bergener <tara.kelly211@gmail.com>
2026-03-14 10:55:41 -0400

    Created routing files for messages, contacts, and documents.

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var logger = require("morgan");
 
 // import the routing file to handle the default (index) route
 var index = require("./server/routes/app");
+const messageRoutes = require('./server/routes/messages');
+const contactRoutes = require('./server/routes/contacts');
+const documentRoutes = require('./server/routes/documents');
 
 // ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ...
 
@@ -44,6 +47,9 @@ app.use(express.static(path.join(__dirname, "dist/cms/browser")));
 
 // Tell express to map the default route ('/') to the index route
 app.use("/", index);
+app.use('/messages', messageRoutes);
+app.use('/contacts', contactRoutes);
+app.use('/documents', documentRoutes);
 
 // ... ADD YOUR CODE TO MAP YOUR URL'S TO ROUTING FILES HERE ...
 
