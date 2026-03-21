b1a0da5bfc2e84bf760ecd83d9a609473e4e8d4e
Tara Bergener <tara.kelly211@gmail.com>
2026-03-21 18:08:16 -0400

    sequence model created.

--- server/models/documents.js
+++ server/models/documents.js
@@ -4,7 +4,7 @@ const documentSchema = mongoose.Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
   url: { type: String },
-  children: [{ type: mongoose.Schema.Types.id, ref: "Document" }],
+  children: [{ type: mongoose.Schema.Types.ObjectId, ref: "Document" }],
 });
 
 module.exports = mongoose.model("Document", documentSchema);
